a85ae9e7e6d86b5b12c0b68564f95540ce482c00
Eric Nutter <nuttereg@gmail.com>
2019-12-06 12:56:38 -0600

    installer updates

---
+++ installer.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# $USER
+# Configure Directory Structure
+cd ~
+
+if [ $HOME/code/ ]; then
+    mv etc code
+else
+    mkdir code
+    mv etc code
+fi
+
+mkdir -p Documents/dox
+mkdir Documents/data
+mkdir .scripts
+
+# important directories
+SCRIPTDIR=$HOME/.scripts/
+CODEDIR=$HOME/code/
+ETCDIR=$HOME/code/etc/
+DOXDIR=$HOME/Documents/dox/
+DATADIR=$HOME/Documents/data/
+
+# create symbolic links for important static files
+ln -s /home/$USER/code/etc/bashrc /home/$USER/.bashrc
+
+# for scripts
+ln -s /home/$USER/code/etc/scripts/se.sh /home/$USER/.scripts/se
+ln -s /home/$USER/code/etc/scripts/dwmbar.sh /home/$USER/.scripts/dwmbar
+ln -s /home/$USER/code/etc/scripts/codes.sh /home/$USER/.scripts/codes
+ln -s /home/$USER/code/etc/scripts/htops.sh /home/$USER/.scripts/htops
+ln -s /home/$USER/code/etc/scripts/pwrs.sh /home/$USER/.scripts/pwrs
+
+for $SCRIPTS in 
