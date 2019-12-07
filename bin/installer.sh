61c699ada4b28a8e628bd1b8d62b66e583efbfcc
Eric Nutter <nuttereg@gmail.com>
2019-12-06 22:32:15 -0500

    more logic to make sure nvim is set up

--- bin/installer.sh
+++ bin/installer.sh
@@ -51,22 +51,37 @@ DATADIR=$HOME/Documents/data/
 echo "\n CHECKING FOR BASH BUSINESS \n"
 ln -s $HOME/code/etc/bashrc $HOME/.bashrc
 
-# checking to see if links already exist before retrying
+# checks to see if links already exist before retrying
 echo "\n CHECKING SETTING UP SUCKLESS SUCKLESS CONFIGS \n"
 if [ ! -f $SUCKDIR/dwm/config.h ]; then
+	echo " SET UP DWM CONFIG \n"
 	ln -s $HOME/code/etc/dwm.h $HOME/code/suck/dwm/config.h
 fi
 if [ ! -f $SUCKDIR/dmenu/config.h ]; then
+	echo " SET UP DMENU CONFIG \n"
 	ln -s $HOME/code/etc/dmenu.h $HOME/code/suck/dmenu/config.h
 fi
 if [ ! -f $SUCKDIR/st/config.h ]; then
+	echo " SET UP ST CONFIG \n"
 	ln -s $HOME/code/etc/st.h $HOME/code/suck/st/config.h
+fi
+
+echo "\n CHECKING TO SEE IF NEOVIM IS SET UP \n"
+if [ ! -d $HOME/.config/nvim ]; then
+	echo "\n SETTING UP NVIM LINK IN XDG_CONFIG_HOME \n"
+	mkdir -p $HOME/.config/nvim
+	ln -s $HOME/code/etc/editor/init.vim $HOME/.config/nvim/init.vim
+else
+	if [ ! -f $HOME/.config/nvim/init.vim ]; then
+		echo "\n SETTING UP NVIM LINK IN XDG_CONFIG_HOME \n"
+		ln -s $HOME/code/etc/editor/init.vim $HOME/.config/nvim/init.vim
+	fi
 fi
 
 # for scripts
 # later this will be managed by file_manager.py
-ln -s /home/$USER/code/etc/scripts/se.sh /home/$USER/.scripts/se
-ln -s /home/$USER/code/etc/scripts/dwmbar.sh /home/$USER/.scripts/dwmbar
-ln -s /home/$USER/code/etc/scripts/codes.sh /home/$USER/.scripts/codes
-ln -s /home/$USER/code/etc/scripts/htops.sh /home/$USER/.scripts/htops
-ln -s /home/$USER/code/etc/scripts/pwrs.sh /home/$USER/.scripts/pwrs
+# ln -s /home/$USER/code/etc/scripts/se.sh /home/$USER/.scripts/se
+# ln -s /home/$USER/code/etc/scripts/dwmbar.sh /home/$USER/.scripts/dwmbar
+# ln -s /home/$USER/code/etc/scripts/codes.sh /home/$USER/.scripts/codes
+# ln -s /home/$USER/code/etc/scripts/htops.sh /home/$USER/.scripts/htops
+# ln -s /home/$USER/code/etc/scripts/pwrs.sh /home/$USER/.scripts/pwrs
