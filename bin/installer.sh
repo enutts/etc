288d4cbcd91e87d809e7817949838c9945ac8cfb
Eric Nutter <nutts@pop-os.localdomain>
2020-11-29 21:20:34 -0600

    cleaned up some, but mostly added script management

--- bin/installer.sh
+++ bin/installer.sh
@@ -7,25 +7,27 @@
 cd ~
 
 echo -e "\n INSTALLING REQUIRED PACKAGES \n"
-sudo apt update && sudo apt upgrade -y 
 # add support for starship
-sudo apt install neovim curl fzf tmux alacritty zsh
+TO_INST=""
+nvim -h  > /dev/null || $TO_INST+=" neovim"
+curl -h > /dev/null || $TO_INST+=" curl"
+fzf -h > /dev/null || $TO_INST+=" fzf"
+tmux -V >  /dev/null || $TO_INST+=" tmux"
+alacritty -h > /dev/null || $TO_INST+=" alacritty"
+zsh --help > /dev/null || $TO_INST+=" zsh"
+
 
 # create directory structure
 # as elegant as they come... i hope
 echo -e " SETTING UP DIRECTORY STRUCTURE  "
-mkdir -p code
-mkdir -p .scripts
+mkdir -p code .scripts
 cd code/etc || mv etc code/
 cd ~
 
+ETC_SCR=$HOME/code/etc/scripts/
+SCRIPTS=$HOME/.scripts
 
 # Alacritty
-echo -e "\n CHECKING FOR ALACRITTY \n"
-if [ ! -e /usr/bin/alacritty ]; then
-    echo -e " -- INSTALLING ALACRITTY -- \n"
-    sudo apt install alacritty -y
-fi
 echo -e "\n CHECKING FOR ALACRITTY CONFIGS \n"
 if [ ! -e $HOME/.config/alacritty/alacritty.yml ]; then
     echo -e " -- CONFIGURING ALACRITTY -- \n"
@@ -47,12 +49,8 @@ if [ ! -e $HOME/.config/starship/starship.toml ]; then
     ln -s $HOME/code/etc/shell/starship.toml $HOME/.config/starship/starship.toml
 fi
 
+
 # Neovim
-echo -e "\n CHECKING FOR NEOVIM \n"
-if [ ! -e /usr/bin/nvim ]; then
-    echo -e "-- INSTALLING NEOVIM --\n" 
-    sudo apt install -y neovim
-fi
 echo -e "\n CHECKING FOR NVIM CONFIGS \n"
 if [ ! -e $HOME/.config/nvim/init.vim ]; then
     echo -e " -- SETTING UP NVIM --\n"
@@ -63,13 +61,42 @@ fi
 
 
 # Tmux
-echo -e "\n CHECKING FOR TMUX \n"
-if [ ! -e /usr/bin/tmux ]; then
-    echo -e " -- INSTALLING TMUX -- \n"
-    sudo apt install tmux -y
-fi
 echo -e "\n CHECKING FOR TMUX CONFIG \n"
 if [ ! -e $HOME/.tmux.conf ]; then
     echo -e " -- CONFIGURING tmux -- \n"
     ln -s $HOME/code/etc/terminal/tmux.conf $HOME/.tmux.conf
 fi
+
+
+# Zsh
+echo -e "\n CHECKING FOR ZSH CONFIGS \n "
+if [ ! -e $HOME/.zshenv ]; then
+    echo -e " -- CONFIGURING ZSH -- \n"
+    mkdir -p $HOME/.config/zsh
+    ln -s $HOME/code/etc/shell/zshenv $HOME/.zshenv
+    ln -s $HOME/code/etc/shell/zshrc $HOME/.config/zsh/.zshrc
+    ln -s $HOME/code/etc/shell/zshcompdump $HOME/.config/zsh/.zshcompdump
+fi
+
+
+# install scripts 
+echo -e "\n CHECKING SCRIPTS \n"
+for F in $ETC_SCR/*; do
+    if [ ! -e $ETC_SCR/F ]; then
+        chmod 754 $F
+    fi
+    if [ ! -a $SCRIPTS/F ]; then 
+        ln -s $ETC_SCR/F $SCRIPTS/F
+    fi
+done 
+
+
+# install software
+if [[ $TO_INST == "" ]]; then 
+    echo -e "\n INSTALLING SOFTWARE \n"
+    echo $TO_INST
+    sudo apt update && sudo apt upgrade -y  
+    sudo apt install $TO_INST -y 
+fi
+
+
--- shell/config.fish
+++
@@ -1,6 +0,0 @@
-if status is-interactive
-and not set -q TMUX
-    exec tmux
-end
-
-starship init fish | source 
